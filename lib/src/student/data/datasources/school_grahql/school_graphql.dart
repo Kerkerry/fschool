5aa031a64e780472219a70764b8323dfdb38ad92
Kerkerry <kerrywangamati24@gmail.com>
2025-07-21 11:50:18 +0300

    Graphql to fetch users

---
+++ lib/src/student/data/datasources/school_grahql/school_graphql.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class GraphQLConfig {
+  static HttpLink httpLink = HttpLink(
+    'https://e5521875ebdb.ngrok-free.app/graphql', // Replace with your GraphQL server URL
+  );
+
+  static WebSocketLink websocketLink = WebSocketLink(
+    '', // Replace with your GraphQL WebSocket URL for subscriptions
+  );
+
+  static ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: Link.from([httpLink, websocketLink]), // Combine HTTP and WebSocket links
+      cache: GraphQLCache(store: InMemoryStore()), // Or use HiveStore for persistent caching
+    ),
+  );
+
+  GraphQLClient clientToQuery() {
+    return GraphQLClient(
+      link: httpLink,
+      cache: GraphQLCache(store: InMemoryStore()),
+    );
+  }
+}
+
